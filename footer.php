5889bbf885ce228ee798a40f853b4c9ac3448daf
ankurpandit <ankurpanditce@gmail.com>
2013-03-04 22:25:31 +0530

    footer changes

--- footer.php
+++ footer.php
@@ -15,22 +15,12 @@
 				<li><a href="aboutUs.php">About</a></li>
 				<li><a href="contactUs.php">Contact Us</a></li>
 			</ul>
-			<!-- Counter Code START -->
-			<ul style="float: right;">
-				<li>
-					<a href="http://www.e-zeeinternet.com/" target="_blank">
-					<img src="http://www.e-zeeinternet.com/count.php?page=931825&style=blushdw&nbdigits=8&reloads=1" alt="HTML Hit Counter" border="0" ></a>
-					<br>
-					<a href="http://www.e-zeeinternet.com/" title="HTML Hit Counter" target="_blank" 
-						style="font-family: Geneva, Arial, Helvetica, sans-serif; 
-							   font-size: 10px; 
-							   color: #000000;">
-						Total views
-					</a>
-				</li>
-			<!-- Counter Code END -->
-			</ul>
-			<div id="copyright">&copy; 2013 Genesis Journal - All rights reserved</div>
+
+
+			<div id="copyright">
+                <img src="http://www.e-zeeinternet.com/count.php?page=934955&style=default&nbdigits=9" alt="Web Counter" border="0" >
+                &copy; 2013 Genesis Journal - All rights reserved
+            </div>
 		</div>
 	</div>
 <div class="clear"></div>

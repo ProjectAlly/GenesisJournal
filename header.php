255faf3959891b789212347880dec1843a319067
admin <admin@SonalPC.local.lan>
2013-03-14 15:47:56 +0530

    Header changes..

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<style>
+.header
+{
+font-size: 40px;
+margin-top: -60px;
+margin-left: 120px;
+text-shadow: 0px 2px 0px green;
+}
+</style>
+
 <div class="grid_16" id="top">
     <!-- while implementing search remove the below mentioned comment-->
     <!--<p id="qsm">for search</p>-->
@@ -7,9 +17,9 @@
 
 <div class="grid_16" id="logo">
 	<a href="index.html"><img src="images/logo.png" alt="Genesis Journal"/></a>
-    Genesis Journal
-    <img style="padding-left: 81%" src="images/coverpage.jpg" alt="Pharamgene" />
+    <div class="header">Genesis Journals</div>
 </div>
+    <img style="float:right;  margin-right: 120px; margin-top: 4px;" src="images/coverpage.jpg" alt="Pharamgene" />
 <div class="clear"></div>
 
 <div class="grid_16" id="navigation">
